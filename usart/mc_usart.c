9a834ffbfa19017fff72741011d2bb1d5b1318d1
archer <427705753@qq.com>
2020-09-21 00:04:58 +0800

    2020.09.21

--- usart/mc_usart.c
+++ usart/mc_usart.c
@@ -8,6 +8,64 @@
 
 #include "mc_usart.h"
 
+#define FRAME_SOF  '<'
+#define FRAME_END  '>'
+
+
+const char ASCII_TABLE[]="0123456789ABCDEF";
+
+
+char* U16ToChar(uint16_t val,char *pMsg)
+{
+    *pMsg++=ASCII_TABLE[(val>>12) & 0x0F];
+    *pMsg++=ASCII_TABLE[(val>>8) & 0x0F];
+    *pMsg++=ASCII_TABLE[(val>>4) & 0x0F];
+    *pMsg++=ASCII_TABLE[(val) & 0x0F];
+    
+    return pMsg;
+    
+}
+
+char * U8ToChar(uint8_t val,char *pMsg)
+{
+    *pMsg++=ASCII_TABLE[(val>>4) & 0x0F];
+    *pMsg++=ASCII_TABLE[(val) & 0x0F];
+    return pMsg;
+}
+
+char* DataToChar(char *pData,char *pMsg,uint16_t size)
+{
+    for(int n=0;n<size;++n)
+    {
+        *pMsg++=ASCII_TABLE[pData[n]>>4 & 0x0F];
+        *pMsg++=ASCII_TABLE[pData[n] & 0x0F];
+    }
+    return pMsg;
+}
+
+
+void DataPack(char bCmd,char bPara,char *pMsg,char *pData,uint8_t size)
+{
+    uint8_t CRC=0x00;
+    *pMsg++=FRAME_SOF;
+    *pMsg++=bCmd;
+    *pMsg++=bPara;
+    *pMsg++=ASCII_TABLE[(size>>4) & 0x0F];
+    *pMsg++=ASCII_TABLE[(size) & 0x0F];
+    
+    for(int n=0;n<size;++n)
+    {
+        *pMsg++=ASCII_TABLE[pData[n]>>4 & 0x0F];
+        *pMsg++=ASCII_TABLE[pData[n] & 0x0F];
+    }
+    
+    //需要使用c生成CRC校验码
+    *pMsg++=FRAME_SOF;
+    
+    
+}
+
+
 
 int main()
 {
